d33a7df5b313ca4c45db1ea42693adbce4bbfa20
plastikSun <marcom2k01@gmail.com>
2023-10-02 15:44:39 +0200

    add: rating, genre attributes, getStarRating() function

--- Models/Production.php
+++ Models/Production.php
@@ -4,11 +4,20 @@ class Production
     public $title;
     public $producer;
     public $language;
+    public $rating;
+    public $genre;
 
-    function __construct(string $title, string $producer, string $language)
+    function __construct(string $title, string $producer, string $language, float $rating, Genre $genre)
     {
         $this->title = $title;
         $this->producer = $producer;
         $this->language = $language;
+        $this->rating = $rating;
+        $this->genre = $genre;
+    }
+
+    public function getStarRating()
+    {
+        return round($this->rating / 2, 2);
     }
 }
