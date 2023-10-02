8197921bcea9cf32ed5c0df2d34505e8436a7ee1
plastikSun <marcom2k01@gmail.com>
2023-10-02 16:01:52 +0200

    add: TvSeries class

--- Models/TvSerie.php
+++ Models/TvSerie.php
@@ -4,4 +4,26 @@ include_once __DIR__ . "/Genre.php";
 
 class TvSerie extends Production
 {
+    public $aired_from_year;
+    public $aired_to_year;
+    public $number_of_episodes;
+    public $number_of_seasons;
+
+    public function __construct(
+        string $title,
+        string $producer,
+        string $language,
+        float $rating,
+        Genre $genre,
+        int $aired_from_year,
+        int $aired_to_year,
+        int $number_of_episodes,
+        int $number_of_seasons,
+    ) {
+        parent::__construct($title, $producer, $language, $rating, $genre);
+        $this->aired_from_year = $aired_from_year;
+        $this->aired_to_year = $aired_to_year;
+        $this->number_of_episodes = $number_of_episodes;
+        $this->number_of_seasons = $number_of_seasons;
+    }
 }
--- index.php
+++ index.php
@@ -6,8 +6,10 @@ include_once __DIR__ . "/Models/Production.php";
 include_once __DIR__ . "/Models/TvSerie.php";
 
 $movie_1 = new Movie("La Città Incantata", "Hayao Miyazaki", "JP", "10", new Genre(["Fantasy", "Adventure"]), 2001, 125);
+$series = new TvSerie("Modern Family", "Christopher Lloyd", "EN", "8", new Genre(["Comedy"]), 2009, 2020, 250, 11);
 
 var_dump($movie_1);
+var_dump($series);
 
 ?>
 
@@ -31,6 +33,11 @@ var_dump($movie_1);
                     ?>
                 </li>
 
+                <li>
+                    <?php
+                    echo "<b>{$series->title}: </b> {$series->getStarRating()} &#9733";
+                    ?>
+                </li>
             </ol>
         </div>
     </main>
